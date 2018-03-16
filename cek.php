ecbe98320cdacb23f7bfbb2404c1dae0366595eb
HanFarhan <30460060+HanFarhan@users.noreply.github.com>
2018-03-16 14:58:32 +0700

    Add files via upload

---
+++ cek.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+  <?php
+
+ print ("Wow! I can use PHP in blogger now...! <br/>");
+ print ("Thanks to <a href='http://premium-stuff-guru.blogspot.com'>PremiumStuffGuru</a>");
+  ?>
+
+</body>
+
+</html>
