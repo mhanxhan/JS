0b6a462a677782da3d8bb157055ab8592e71d4cb
HanFarhan <30460060+HanFarhan@users.noreply.github.com>
2018-07-23 04:32:51 +0000

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -30,7 +30,7 @@ if(x=="6"){xxx="/2017/12/creating-back-button-to-previous.html"}
 if(x=="7"){xxx="/2018/03/download-super-seo-template.html"}
 if(x=="8"){xxx="/2018/03/earn-money-from-shortlink.html"}
 if(x=="9"){xxx="/2018/03/script-anti-copas.html"}
-if(x=="10"){xxx="/2018/03/how-to-eliminate-rust-parts-of-chrome.html"}a_to_ve[i].href="https://www.hanmodder.tk"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="10"){xxx="/2018/03/how-to-eliminate-rust-parts-of-chrome.html"}a_to_ve[i].href="https://safelinkuyy.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
